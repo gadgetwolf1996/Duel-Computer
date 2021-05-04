4a67a4ccc0e6edbb61b12a8e57ec21db0657beaa
gadgetwolf1996 <gadgetwolf1996@outlook.com>
2021-05-04 19:26:34 +0100

    Formats Monster effect of Pendulum monsters. Alter text sizing or flavor text and effect text. Fixed incorrect Font for Race and Flavour. Added if statements for all templates. Altered card types to include "Effect" except extra deck monsters.

--- DuelComputer.js
+++ DuelComputer.js
@@ -109,12 +109,21 @@ function getCardData(cardData = -1){
   //set card information variables
   id = user.id;
   name = user.name;
-  type = user.type.replace(' Card', '').replace('Flip ', '').replace('Toon', 'Effect').replace('Spirit', 'Effect').replace('Union ', '').replace('Gemini', 'Effect');
-  if (type.includes("Pendulum") || type.includes("Synchro") || type.includes("Normal") || type.includes("Effect")) {
-    type = type.replace('Tuner ', '');
-  }
-  else {
-    type = type.replace('Tuner', 'Effect');
+  type = user.type;
+  switch (type) {
+    case 'Tuner Monster':
+      type = type.replace('Tuner', 'Tuner Effect');
+      break;
+    case 'Toon Monster':
+      type = type.replace('Toon', 'Toon Effect');
+      break;
+    case 'Spirit Monster':
+      type = type.replace('Spirit', 'Spirit Effect');
+      break;
+    case 'Gemini Monster':
+      type = type.replace('Gemini', 'Gemini Effect');
+    default:
+      break;
   }
   desc = user.desc;
   race = user.race;
@@ -145,13 +154,13 @@ function getCardData(cardData = -1){
     var temp = type;
     temp = temp.replace(' Monster', '');
     var templist = temp.split(' ');
-    document.getElementById("Race").innerHTML = "[ " + race + " / ";
+    document.getElementById("Race").innerHTML = "[" + race + "/";
     for (let index = 0; index < templist.length; index++) {
       if (index == templist.length - 1) {
-        document.getElementById("Race").innerHTML += templist[index] + " ]";
+        document.getElementById("Race").innerHTML += templist[index] + "]";
       }
       else {
-        document.getElementById("Race").innerHTML += templist[index] + " / ";
+        document.getElementById("Race").innerHTML += templist[index] + "/";
       }
     }
   }
@@ -167,20 +176,36 @@ function getCardData(cardData = -1){
   else {
     document.getElementById("Attribute").src = "/Templates/Attributes/" + type + ".png";
   }
+  var penDesc;
+  if(type.includes("Pendulum")){
+    penDesc = desc.split('<br>----------------------------------------<br>')[0].replace('[ Pendulum Effect ]<br>', '');
+    if(type.includes("Normal")){
+      desc = desc.split('<br>----------------------------------------<br>')[1].replace('[ Flavor Text ]<br>', '');
+    }
+    else{
+      desc = desc.split('<br>----------------------------------------<br>')[1].replace('[ Monster Effect ]<br>', '');
+    }
+  }
   
-  if (type != "Normal Tuner Monster" && type != "Normal Monster" && type != "Token") {
+  if (type.includes("Normal") || type.includes("Token")) {
+    document.getElementById("Effect").innerHTML = desc;
+    document.getElementById("Effect").style.fontFamily = "Matrix Flavour";
+  }
+  else{
     document.getElementById("Effect").style.fontFamily = "Matrix";
     document.getElementById("Effect").innerHTML = analyseDesc(desc, type);
     
   }
-  else{
-    document.getElementById("Effect").innerHTML = desc;
-    document.getElementById("Effect").style.fontFamily = "Matrix Flavour";
-  }
 
 
   //font size alteration
-  var returnedVar = scaleFont(desc.length);
+  var returnedVar;
+  if(type.includes("Normal")){
+    returnedVar = scaleFont(desc.length, 16);
+  }
+  else{
+    returnedVar = scaleFont(desc.length, descFontSize);
+  }
   document.getElementById('Effect').style.fontSize = returnedVar[0] +'px';
   document.getElementById('Effect').style.lineHeight = returnedVar[1]+'px';
   /*
@@ -243,11 +268,72 @@ function getCardData(cardData = -1){
   img.src = image;
 
   let blankImg = document.getElementById("blankImg");
-  if (type == "Ritual Effect Monster") {
-    blankImg.src = "/Templates/"+type.replace(' Effect', '')+".png";
+  
+  if(type.includes('Pendulum')){
+    if(type.includes('Effect')){
+      if(type.includes('Fusion')){
+        blankImg.src = '/Templates/Pendulum Effect Fusion Monster.png';
+      }
+      else{
+        if(type.includes('Synchro')){
+          blankImg.src = '/Templates/Synchro Pendulum Effect Monster.png';
+        }
+        else{
+          if(type.includes('XYZ')){
+            blankImg.src = '/Templates/Xyz Pendulum Effect Monster.png';
+          }
+          else{
+            blankImg.src = '/Templates/Pendulum Effect Monster.png';
+          }
+        }
+      }
+    }
+    else{
+      blankImg.src = '/Templates/Pendulum Normal Monster.png';
+    }
   }
-  else {
-    blankImg.src = "/Templates/"+type+".png";
+  else{
+    if(type.includes('Normal')){
+      blankImg.src = '/Templates/Normal Monster.png';
+    }
+    else{
+      if(type.includes('Fusion')){
+        blankImg.src = '/Templates/Fusion Monster.png';
+      }
+      else{
+        if(type.includes('Link')){
+          blankImg.src = '/Templates/Link Monster.png';
+        }
+        else{
+          if(type.includes('Ritual')){
+            blankImg.src = '/Templates/Ritual Monster.png';
+          }
+          else{
+            if(type.includes('Spell')){
+              blankImg.src = '/Templates/Spell.png';
+            }
+            else{
+              if(type.includes('Trap')){
+                blankImg.src = '/Templates/Trap.png';
+              }
+              else{
+                if(type.includes('Synchro')){
+                  blankImg.src = '/Templates/Synchro Monster.png';
+                }
+                else{
+                  if(type.includes('XYZ')){
+                    blankImg.src = '/Templates/Xyz Monster.png';
+                  }
+                  else{
+                    blankImg.src = '/Templates/Effect Monster.png';
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   }
   //const favicon = document.getElementById("favicon"); 
   //favicon.setAttribute("href", image_icon);
@@ -301,7 +387,7 @@ function analyseDesc(desc, type){
     }
   
     if (edmon) {
-      return "<font class=\"EdSummonCon\">" + edSummonCon + "</font>" + "<br><font class=\"Effect\">" + htmlConversion + "</font>";
+      return "<font class=\"EdSummonCon\"> <span>" + edSummonCon + "</span></font>" + "<br><font class=\"Effect\">" + htmlConversion + "</font>";
     }
     else {
       return "<font class=\"Effect\">" + htmlConversion + "</font>";
@@ -315,7 +401,7 @@ function ActivationConditions(){
   if (concheck){
       context = effectSegment.split(":");
       effectSegment = context[1];
-      context = "<font style=color:green>" + context[0] + ": " + "</font>";
+      context = "<span style=color:green>" + context[0] + ": " + "</span>";
     }
     return context;
 }
@@ -403,12 +489,17 @@ function scaleTitleFont(titleLength){
   return scaledTitleFont;
 }
 
-function scaleFont(descLength){
-  var currentValues= new Array(descFontSize, descLineHeight);
+function scaleFont(descLength, defaultFontSize){
+  var currentValues= new Array(defaultFontSize, descLineHeight);
   console.log("---Font Scaling Start---");
-  while(calcRowsUsed(currentValues[0], currentValues[1], descLength, true) <= -1.5){
-    currentValues[1] = currentValues[1] - 1;
-    currentValues[0] = currentValues[0] - 1;
+  while(calcRowsUsed(currentValues[0], currentValues[1], descLength, true) <= -1.1){
+    if(currentValues[1] % 1 == 0.5){
+      currentValues[1] = currentValues[1] - 0.5;
+    }
+    else{
+      currentValues[1] = currentValues[1] - 0.5;
+      currentValues[0] = currentValues[0] - 1;
+    }
     console.log("New Font Size: " + currentValues[0] + " | New Line Height: " + currentValues[1]);
     console.log("---Next Font Scaling---");
   }
